1bfa9a142fca852e84533df7c0bfcf9918f3a194
Chase Ripplinger <chase@ripply.io>
2022-06-28 16:28:01 -0700

    Add derivation path for Qnekt

--- truffle-config.js
+++ truffle-config.js
@@ -5,19 +5,31 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      // provider: () => new HDWalletProvider("frost shop innocent nothing affair length news hold tray pride wash car", "http://localhost:23889"),
-      provider: () => new HDWalletProvider("frost shop innocent nothing affair length news hold tray pride wash car", "ws://localhost:23889"),
+      provider: () => new HDWalletProvider({
+        mnemonic: "",
+        providerOrUrl: "ws://localhost:23889",
+        // providerOrUrl: "http://localhost:23889",
+        // derivationPath: "m/44'/2301'/0'/0/1",
+      }),
       network_id: "*",
       // from: "0x7926223070547d2d15b2ef5e7383e541c338ffe9",
       gasPrice: "0x5d21dba000"
     },
     testnet: {
-      provider: () => new HDWalletProvider("frost shop innocent nothing affair length news hold tray pride wash car", "https://testnet-janus.qiswap.com/api/"),
+      provider: () => new HDWalletProvider({
+        mnemonic: "",
+        providerOrUrl: "https://testnet-janus.qiswap.com/api/",
+        // derivationPath: "m/44'/2301'/0'/0/1",
+      }),
       network_id: "*",
       gasPrice: "0x5d21dba000"
     },
     mainnet: {
-      provider: () => new HDWalletProvider("frost shop innocent nothing affair length news hold tray pride wash car", "https://janus.qiswap.com/api/"),
+      provider: () => new HDWalletProvider({
+        mnemonic: "",
+        providerOrUrl: "https://janus.qiswap.com/api/",
+        // derivationPath: "m/44'/2301'/0'/0/1"),
+      }),
       network_id: "*",
       gasPrice: "0x5d21dba000"
     },
