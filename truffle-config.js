d138515a6a8f14cec9d2a99ddea84e2d3fd64809
Chase Ripplinger <chase@ripply.io>
2022-07-07 10:46:29 -0700

    Update hdwallet-provider and tidy up truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -4,22 +4,18 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-    development: {
+    regtest: {
       provider: () => new HDWalletProvider({
         mnemonic: "",
-        providerOrUrl: "ws://localhost:23889",
-        // providerOrUrl: "http://localhost:23889",
-        // derivationPath: "m/44'/2301'/0'/0/1",
+        providerOrUrl: "http://localhost:23889",
       }),
       network_id: "*",
-      // from: "0x7926223070547d2d15b2ef5e7383e541c338ffe9",
       gasPrice: "0x5d21dba000"
     },
     testnet: {
       provider: () => new HDWalletProvider({
         mnemonic: "",
         providerOrUrl: "https://testnet-janus.qiswap.com/api/",
-        // derivationPath: "m/44'/2301'/0'/0/1",
       }),
       network_id: "*",
       gasPrice: "0x5d21dba000"
@@ -28,7 +24,6 @@ module.exports = {
       provider: () => new HDWalletProvider({
         mnemonic: "",
         providerOrUrl: "https://janus.qiswap.com/api/",
-        // derivationPath: "m/44'/2301'/0'/0/1"),
       }),
       network_id: "*",
       gasPrice: "0x5d21dba000"
